9478bac4aa856144df0869c56cac129659183c53
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 21:43:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function createAnimal(animalType) {
 
   function dangerousAnimals(deadlyDevice) {
     return {
-      animalType: 'animalType',
-      deadlyDevice: 'deadlyDevice'
+      [animalType] = 'animalType',
+      [deadlyDevice] = 'deadlyDevice'
     }
 }
 return dangerousAnimals
